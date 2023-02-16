f843177bc3b69c095e298ae74b74280fcc7b48df
Dulen <isaaksdulen@gmail.com>
2023-02-16 14:00:34 -0800

    fout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programmin is like building a multilingual
+ * puzzle,followed by newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programmin il like building a multilingual puzzle");
+
+	return(0);
+}
